cfeb01b0a7faf9ff193a8df3c077b5aaf9accf50
Prudhvi <medap@PrudhvisPC>
2023-06-12 17:23:37 +0530

    6th

---
+++ build/precache-manifest.a325699a541b68a6a2225f26f42d6d7c.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "32302b755c63b5d91eae087817c9c7cf",
+    "url": "/Notes-App/index.html"
+  },
+  {
+    "revision": "ad9e9bd84b4e775138b0",
+    "url": "/Notes-App/static/js/2.113795aa.chunk.js"
+  },
+  {
+    "revision": "f901ff82e32ce9380c93",
+    "url": "/Notes-App/static/js/main.9396246e.chunk.js"
+  },
+  {
+    "revision": "0167b9f8492d48145a16",
+    "url": "/Notes-App/static/js/runtime-main.f1e2d96e.js"
+  }
+]);
